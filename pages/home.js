578eeaee3f09111cd1f7e2c1bcbe945fa521e56f
dnkc <dnkcdesign@gmail.com>
2022-04-20 21:43:29 -0500

    it didnt fix it

--- pages/home.js
+++ pages/home.js
@@ -32,11 +32,7 @@ function Home({ providers }) {
               <div className="pl-4">
                 <button
                   className="text-blue-700 font-semibold rounded-full border border-blue-700 px-5 py-1.5 transition-all hover:border-2"
-                  onClick={() =>
-                    signIn(provider.id, {
-                      callbackUrl: `${VERCEL_URL}/api/auth/callback/google/`,
-                    })
-                  }
+                  onClick={() => signIn(provider.id, { callbackUrl: "/" })}
                 >
                   Sign in
                 </button>
